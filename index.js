2b671f1bdc90d3ceba44e7a4f428b2fd5bb6db92
Your User Name <dicksonmwadime2020@gmail.com>
2023-04-02 16:50:28 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer
+const leastFavoriteCustomer = 'chriss'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+upperCaseCustomerName()
+
+function setBestCustomer () {
+    bestCustomer = 'not bob'
+}
+
+setBestCustomer();
+
+function overwriteBestCustomer (newName) {
+    bestCustomer = newName
+}
+
+overwriteBestCustomer('maybe bob');
+
+function changeLeastFavoriteCustomer (newName) {
+    leastFavoriteCustomer = newName
+}
+
+changeLeastFavoriteCustomer('new name');
+console.log(bestCustomer);
